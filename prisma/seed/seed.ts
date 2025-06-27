068b6d0f51027b7780dd63a3ab7b404ba73aa2a9
Duc Huy Nguyen <duchuytv2102@gmail.com>
2025-06-27 16:32:23 +0700

    feat(seed): seeded the database with Image data for classifieds - created an images seed process linking placeholder to each classified - connected images in the prisma schema by classifiedId - used thumbhash/unlazy to generate a base64 blur placeholder - stored the placeholder string in the database for each image

--- prisma/seed/seed.ts
+++ prisma/seed/seed.ts
@@ -1,13 +1,15 @@
 import { PrismaClient } from '@prisma/client';
 import { seedTaxonomy } from './taxonomy.seed';
 import { seedClassified } from './classified.seed';
+import { seedImages } from './images.seed';
 
 const prisma = new PrismaClient();
 
 async function main() {
     // await prisma.$executeRaw`truncate table "makes", "models" restart identity cascade`;
     // await seedTaxonomy(prisma);
-    await seedClassified(prisma);
+    // await seedClassified(prisma);
+    await seedImages(prisma);
 }
 
 main()

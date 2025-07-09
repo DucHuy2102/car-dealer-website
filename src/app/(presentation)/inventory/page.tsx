62b956dff46037a4701f1b87dc1b0df91854f58e
Duc Huy Nguyen <duchuytv2102@gmail.com>
2025-07-09 16:33:51 +0700

    feat: added Sidebar & created custom Pagination components - Created a Sidebar for advanced filters - Built reusable Pagination component - Implemented navigation, page number links and ellipsis - Used URL query to manage current page - Added .env file and reference with a typed approach using t3-oss/env

--- src/app/(presentation)/inventory/page.tsx
+++ src/app/(presentation)/inventory/page.tsx
@@ -1,25 +1,77 @@
 import ClassifiedsList from '@/components/inventory/classified-list';
+import CustomSidebar from '@/components/inventory/sidebar';
+import { CustomPagination } from '@/components/shared/custom-pagination';
+import { CLASSIFIEDS_PER_PAGE } from '@/config/constants';
+import { routes } from '@/config/routes';
 import { AwaitedPageProps, Favourites, PageProps } from '@/config/types';
 import { prisma } from '@/lib/prisma';
 import { redisStore } from '@/lib/redis-store';
 import { getSourceId } from '@/lib/source-id';
+import { z } from 'zod';
 
 const getInventory = async (searchParams: AwaitedPageProps['searchParams']) => {
+    const pageSchema = z
+        .string()
+        .transform((value) => Math.max(Number(value), 1))
+        .optional();
+    const validPage = pageSchema.parse(searchParams?.page);
+    const page = validPage ? validPage : 1;
+    const offset = (page - 1) & CLASSIFIEDS_PER_PAGE;
+
     return await prisma.classified.findMany({
+        where: {},
         include: {
-            images: true,
+            images: { take: 1 },
         },
+        skip: offset,
+        take: CLASSIFIEDS_PER_PAGE,
     });
 };
 
 export default async function InventoryPage(props: PageProps) {
     const searchParams = await props.searchParams;
     const classifieds = await getInventory(searchParams);
+    const count = await prisma.classified.count({
+        where: {},
+    });
+    const totalPages = Math.ceil(count / CLASSIFIEDS_PER_PAGE);
 
     const sourceId = await getSourceId();
     const favourites = await redisStore.get<Favourites>(sourceId ?? '');
 
     return (
-        <ClassifiedsList classifieds={classifieds} favourites={favourites ? favourites.ids : []} />
+        <div className='flex'>
+            <CustomSidebar minMaxValues={null} searchParams={searchParams} />
+
+            <div className='flex-1 p-4 bg-white'>
+                <div className='flex space-y-2 flex-col items-center justify-center pb-4 -mt-1'>
+                    <div className='flex justify-between items-center w-full'>
+                        <h2 className='text-sm md:text-base lg:text-lg font-semibold min-w-fit'>
+                            We have found{' '}
+                            <span className='text-white bg-primary rounded-full px-3 pb-0.5'>
+                                {count}
+                            </span>{' '}
+                            classifieds
+                        </h2>
+                        {/* <DialogFilters /> */}
+                    </div>
+                    <CustomPagination
+                        baseUrl={routes.inventory}
+                        totalPages={totalPages}
+                        styles={{
+                            paginationRoot: 'flex justify-end',
+                            paginationPrevious: '',
+                            paginationNext: '',
+                            paginationLink: 'border-none active:border',
+                            paginationLinkActive: '',
+                        }}
+                    />
+                    <ClassifiedsList
+                        classifieds={classifieds}
+                        favourites={favourites ? favourites.ids : []}
+                    />
+                </div>
+            </div>
+        </div>
     );
 }
---
+++ src/components/shared/custom-pagination.tsx
@@ -0,0 +1,157 @@
+'use client';
+
+import {
+    Pagination,
+    PaginationContent,
+    PaginationItem,
+    PaginationLink,
+    PaginationNext,
+    PaginationPrevious,
+} from '@/components/ui/pagination';
+import { env } from '@/env';
+import { cn } from '@/lib/utils';
+import { useQueryState } from 'nuqs';
+import { useEffect, useState } from 'react';
+
+interface PaginationProps {
+    baseUrl: string;
+    totalPages: number;
+    maxVisiblePages?: number;
+    styles: {
+        paginationRoot: string;
+        paginationPrevious: string;
+        paginationNext: string;
+        paginationLink: string;
+        paginationLinkActive: string;
+    };
+}
+
+export function CustomPagination(props: PaginationProps) {
+    const { baseUrl, totalPages, maxVisiblePages = 5, styles } = props;
+    const [curentPage, setCurrentPage] = useQueryState('page', {
+        defaultValue: 1,
+        parse: (value) => {
+            const parsedValue = Number.parseInt(value, 10);
+            return Number.isNaN(parsedValue) || parsedValue < 1 ? 1 : parsedValue;
+        },
+        serialize: (value) => value.toString(),
+        shallow: false,
+    });
+    const [visibleRange, setVisibleRange] = useState({
+        start: 1,
+        end: Math.min(maxVisiblePages, totalPages),
+    });
+
+    useEffect(() => {
+        const halfVisible = Math.floor(maxVisiblePages / 2);
+        const newStart = Math.max(
+            1,
+            Math.min(curentPage - halfVisible, totalPages - maxVisiblePages + 1)
+        );
+        const newEnd = Math.min(totalPages, newStart + maxVisiblePages - 1);
+        setVisibleRange({
+            start: newStart,
+            end: newEnd,
+        });
+    }, [curentPage, totalPages, maxVisiblePages]);
+
+    const createPageUrl = (pageNumber: number) => {
+        const url = new URL(baseUrl, env.NEXT_PUBLIC_APP_URL);
+        url.searchParams.set('page', pageNumber.toString());
+        return url.toString();
+    };
+
+    const handleEllipsisClick = (direction: 'left' | 'right') => {
+        const left = Math.max(1, visibleRange.start - maxVisiblePages);
+        const right = Math.min(totalPages, visibleRange.end + maxVisiblePages);
+        const newPage = direction === 'left' ? left : right;
+        setCurrentPage(newPage);
+    };
+
+    return (
+        <Pagination className={styles.paginationRoot}>
+            <PaginationContent className='lg:gap-4'>
+                <PaginationItem>
+                    <PaginationPrevious
+                        href={createPageUrl(curentPage - 1)}
+                        className={cn(curentPage === 1 && 'hidden', styles.paginationPrevious)}
+                        onClick={(e) => {
+                            e.preventDefault();
+                            if (curentPage > 1) setCurrentPage(curentPage - 1);
+                        }}
+                    />
+                </PaginationItem>
+
+                {visibleRange.start > 1 && (
+                    <PaginationItem className='hidden lg:block'>
+                        <PaginationLink
+                            href='#'
+                            className={styles.paginationLink}
+                            onClick={(e) => {
+                                e.preventDefault();
+                                handleEllipsisClick('left');
+                            }}
+                        >
+                            ...
+                        </PaginationLink>
+                    </PaginationItem>
+                )}
+
+                {Array.from(
+                    { length: visibleRange.end - visibleRange.start + 1 },
+                    (_, index) => visibleRange.start + index
+                ).map((pageNumber) => {
+                    const isActive = pageNumber === curentPage;
+                    let rel = '';
+                    if (pageNumber === curentPage - 1) rel = 'prev';
+                    if (pageNumber === curentPage + 1) rel = 'next';
+                    return (
+                        <PaginationItem key={pageNumber}>
+                            <PaginationLink
+                                isActive={isActive}
+                                {...(rel ? { rel } : {})}
+                                href={createPageUrl(pageNumber)}
+                                className={cn(
+                                    styles.paginationLink,
+                                    isActive && styles.paginationLinkActive
+                                )}
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    if (!isActive) setCurrentPage(pageNumber);
+                                }}
+                            >
+                                {pageNumber}
+                            </PaginationLink>
+                        </PaginationItem>
+                    );
+                })}
+
+                {visibleRange.end < totalPages && (
+                    <PaginationItem className='hidden lg:block'>
+                        <PaginationLink
+                            href='#'
+                            className={styles.paginationLink}
+                            onClick={(e) => {
+                                e.preventDefault();
+                                handleEllipsisClick('right');
+                            }}
+                        >
+                            ...
+                        </PaginationLink>
+                    </PaginationItem>
+                )}
+
+                <PaginationItem>
+                    <PaginationNext
+                        href={createPageUrl(curentPage + 1)}
+                        className={cn(curentPage >= totalPages && 'hidden', styles.paginationNext)}
+                        onClick={(e) => {
+                            e.preventDefault();
+                            if (curentPage < totalPages) setCurrentPage(curentPage + 1);
+                        }}
+                    />
+                </PaginationItem>
+            </PaginationContent>
+        </Pagination>
+    );
+}
